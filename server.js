7e43bd5cc2bd161520435cb953d4947a18601fee
habib <sarajlic.habib@gmail.com>
2023-07-03 12:35:26 +0200

    test

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const Gpio = require('onoff').Gpio;
+
+let relej = new Gpio(4, 'out');
+relej.writeSync(1);
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -22,17 +27,22 @@ app.get('/gate/health', async (req, res) => {
 });
 
 app.get('/gate/open', async (req, res) => {
-    //counter++;
-    res.json("gate is opening: " + counter);
+    relej.writeSync(0);
+        setTimeout(()=>{relej.writeSync(1);}, 2000);
+
+    res.json("Gate is opening");
 });
 
-app.get('/gate/guest-invite', async (req, res) => {
-    res.sendFile('HTML/testgpt.html', {root: __dirname });
-});
+app.get('/gate/open/self-close', async (req, res) => {
+    relej.writeSync(0);
+        setTimeout(()=>{relej.writeSync(1);}, 2000);
 
+    res.sendFile('HTML/test-self-close.html', {root: __dirname });
+});
+/*
 app.get('/gate/open-test', async (req, res) => {
     counter++;
     console.log("EVO: " + counter)
     res.json("gate is opening: " + counter);
-});
+});*/
 app.listen(port, '0.0.0.0', () => console.log(`Hello world app listening on port ${port}!`));
